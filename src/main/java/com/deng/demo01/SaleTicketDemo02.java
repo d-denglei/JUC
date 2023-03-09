2ea2b4c6567c6ae2d77072a5ca03332cc60ea6a5
dengl <dengl@voicecomm.cn>
2023-03-09 21:50:58 +0800

    no message

---
+++ src/main/java/com/deng/demo01/SaleTicketDemo02.java
@@ -0,0 +1,63 @@
+package com.deng.demo01;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class SaleTicketDemo02 {
+    public static void main(String[] args) {
+        //并发:多个线程操作同一个资源， 把资源类丢入线程
+        Ticket2 ticket2 = new Ticket2();
+
+        //@FunctionalInterface 匿名内部类 jdk1.8 lambda表达式 (参数)->{具体代码}
+        new Thread(() -> {
+            for (int i = 1; i < 40; i++) {
+                ticket2.sale();
+            }
+        }, "A").start();
+
+        new Thread(() -> {
+            for (int i = 1; i < 40; i++) {
+                ticket2.sale();
+            }
+        }, "B").start();
+
+        new Thread(() -> {
+            for (int i = 1; i < 40; i++) {
+                ticket2.sale();
+            }
+        }, "C").start();
+    }
+}
+
+//lock
+//资源类
+class Ticket2 {
+
+    //票数
+    private int number = 30;
+
+    //官方文档 使用常用语法
+    // Lock l = ...;
+    // l.lock();
+    // try { // access the resource protected by this lock }
+    // finally { l.unlock(); }
+
+    Lock lock = new ReentrantLock();
+
+    //卖票的方式
+    public void sale() {
+        lock.lock();
+        lock.tryLock();
+        try {
+            if (number > 0) {
+                System.out.println(Thread.currentThread().getName()
+                        + "卖出了" + (number--) + "票:剩余：" + number);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();//解锁
+        }
+
+    }
+}
---
+++ src/main/java/com/deng/product_consumer/A.java
@@ -0,0 +1,83 @@
+package com.deng.product_consumer;
+
+/**
+ * 线程之间的通信问题：生产者和消费者问题
+ * 线程交替执行 A B操作同一个变量 num=0
+ * A: num+1
+ * B: num-1
+ */
+
+public class A {
+    public static void main(String[] args) {
+        Data data = new Data();
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    data.increment();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "A").start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    data.decrement();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "B").start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    data.increment();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "C").start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    data.decrement();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "D").start();
+    }
+}
+
+//资源类
+//判断等待，业务，通知
+class Data {
+    private int number = 0;
+
+    //+1
+    public synchronized void increment() throws InterruptedException {
+        if (number != 0) {
+            //等待
+            this.wait();
+        }
+        number++;
+        System.out.println(Thread.currentThread().getName() + "=>" + number);
+        //通知其他线程 我+1完毕
+        this.notifyAll();
+    }
+
+    //-1
+    public synchronized void decrement() throws InterruptedException {
+        if (number == 0) {
+            //等待
+            this.wait();
+        }
+        number--;
+        System.out.println(Thread.currentThread().getName() + "=>" + number);
+        //通知其他线程 我-1完毕
+        this.notifyAll();
+    }
+}
